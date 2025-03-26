b329cd0494920c28d23b13c9b21594440efa95dd
kn-srini-gen-ai <knsrini9@gmail.com>
2025-03-26 12:58:50 +0530

    Update sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -3,6 +3,7 @@ import requests
 headers = {"Content-Type": "application/json"}
 url = "http://127.0.0.1:8000/add"
 data = {"num1": 15.0, "num2": 3.0}
+# added in f4@main
 
 response = requests.post(url, headers=headers, json=data)
 print("Add Test Response:", response.json())
@@ -49,6 +50,7 @@ data = {"num1": 5.5, "num2": 3.2}
 response = requests.post(url, headers=headers, json=data)
 print("Add Decimal Numbers Test Response:", response.json())
 print(response.status_code)
+#added f4@min
 
 
 
